0c7a3d8bc46d63f7aa1b343163206cc8d99efc4f
TroyMitchell <andrew998@126.com>
2024-06-30 14:36:28 +0800

    modify  to

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.505218.xyz": dockerHub,
+  "quay.505218.xyz": "https://quay.io",
+  "gcr.505218.xyz": "https://gcr.io",
+  "k8s-gcr.505218.xyz": "https://k8s.gcr.io",
+  "k8s.505218.xyz": "https://registry.k8s.io",
+  "ghcr.505218.xyz": "https://ghcr.io",
+  "cloudsmith.505218.xyz": "https://docker.cloudsmith.io",
+  "ecr.505218.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.505218.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
